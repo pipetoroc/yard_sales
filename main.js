c21fca2061e5f0daa2f54c246ac19b183ccab0f4
pipetoroc <ftorocastilla@gmail.com>
2022-08-01 15:20:15 -0300

    feat: fuse menu desktop and mobile, add media queries to show or remove menus

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
    desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+   mobileMenu.classList.toggle('inactive');
+}
+
+
 
 
 
