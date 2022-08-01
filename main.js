b6cbb5d914d7c79c0b62c556a454e2f456e23a86
pipetoroc <ftorocastilla@gmail.com>
2022-08-01 19:15:48 -0300

    feat: fuse navbar in desktop and mobile, use javaScript to hide or show the menus

--- main.js
+++ main.js
@@ -2,16 +2,42 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const cart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu);
+cart.addEventListener('click', toggleShoppingCart);
+
 
 function toggleDesktopMenu() {
+   const isDesktopMenuClosed = productDetail.classList.contains('inactive');
+
+   if (!isDesktopMenuClosed) {
+      productDetail.classList.add('inactive')
+   }
    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+   const isMobileMenuClosed = productDetail.classList.contains('inactive');
+
+   if(!isMobileMenuClosed){
+      productDetail.classList.add('inactive')
+   }
    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart(){
+   const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+   const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+   if(!isMobileMenuClosed || !isDesktopMenuClosed){
+      mobileMenu.classList.add('inactive')
+      desktopMenu.classList.add('inactive')
+   }
+
+   productDetail.classList.toggle('inactive');
 }
 
 
